d9cdca34265c54c79304cf4ae96797d8f1491534
sateeshbaboo <chv.sateesh@gmail.com>
2024-05-12 23:27:16 +0530

    initial commit

---
+++ src/main/java/com/aditya/managesystem/ManagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.managesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/managesystem/ManagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.managesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
